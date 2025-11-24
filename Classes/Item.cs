23965adebe1837b6da753d21a8b502a55c5a7aa9
Anastasia <filippov31012007@gmail.com>
2025-11-24 16:26:05 +0500

    Разработанный класс. Osennikov

---
+++ Classes/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureStore_Osennikov.Classes
+{
+    public class Item
+    {
+        public string name {  get; set; }
+        public int price { get; set; }
+        public string src { get; set; }
+
+        public Item(string name, int price, string src) 
+        {
+            this.name = name;
+            this.price = price;
+            this.src = src;
+        }
+    }
+}
